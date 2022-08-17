dc34c6a067498987d651a7fd7beb821ac2007d6f
Katya <evpankratova.mail@yandex.ru>
2022-08-17 22:48:28 +0300

    new project example005

---
+++ example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите Ваше имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{ 
+    Console.WriteLine("Ура! Эта Машка!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
